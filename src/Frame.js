24758e7e18e336df9770bc4f3c7e5761ba679ec2
dishabl <85846973+dishabl@users.noreply.github.com>
2023-11-27 21:38:13 +0300

    fifth

--- src/Frame.js
+++ src/Frame.js
@@ -7,19 +7,37 @@ export default function Frame() {
   const [textList, setTextList] = useState([]);
   const addText = (text) => {
     if (text.trim() !== "") {
-      setTextList([...textList, text]);
+      setTextList([...textList, { text, isToggled: false }]);
     }
   };
+
+  const removeText = (index) => {
+    const newList = [...textList];
+    newList.splice(index, 1);
+    setTextList(newList);
+  };
+
+  const toggleText = (index) => {
+    const newList = [...textList];
+    newList[index].isToggled = !newList[index].isToggled;
+    setTextList(newList);
+  };
+
   return (
     <div className="Fram">
       <br />
       <h1>Get things done!</h1>
       <br />
+
       <InputForm onTextSubmit={addText} />
       <ul className="ull">
-        {textList.map((text, index) => (
+        {textList.map((item, index) => (
           <li key={index}>
-            <TaskList task={text} />
+            <TaskList
+              task={item}
+              onToggle={() => toggleText(index)}
+              onDelete={() => removeText(item)}
+            />
           </li>
         ))}
       </ul>
--- src/TaskList.js
+++ src/TaskList.js
@@ -3,11 +3,13 @@ import trash from "./trash.png";
 import edit from "./edit.png";
 import { useState } from "react";
 
-export default function TaskList({ task }) {
+export default function TaskList({ task, onToggle, onDelete }) {
   const [isToggled, setIsToggled] = useState(false);
-  const handleClick = () => {
+  const handleToggle = () => {
     setIsToggled((prevState) => !prevState);
+    onToggle();
   };
+  const { text } = task;
   const textDecoration = isToggled ? "line-through" : "none";
   const color = isToggled ? "rgb(117, 117, 117)" : "white";
 
@@ -15,27 +17,21 @@ export default function TaskList({ task }) {
     <div>
       <div
         className="input-group-text taska input-group"
-        onClick={handleClick}
+        onClick={handleToggle}
         style={{ textDecoration, color }}
       >
-        {task}
-        <button className="edit">
-          <img
-            onClick={() => console.log("CLick edit")}
-            // className="edit"
-            src={edit}
-            alt="edit"
-            height="20"
-          />
+        {text}
+        <button className="edit" onClick={(e) => e.stopPropagation()}>
+          <img src={edit} alt="edit" height="20" />
         </button>
-        <button className="trash">
-          <img
-            // className="trash"
-            onClick={() => console.log("CLick trash")}
-            src={trash}
-            alt="trash"
-            height="20"
-          />
+        <button
+          className="trash"
+          onClick={(e) => {
+            e.stopPropagation();
+            onDelete();
+          }}
+        >
+          <img src={trash} alt="trash" height="20" />
         </button>
       </div>
     </div>
