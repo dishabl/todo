c27677fb328464d0844a8b92e4f6eb0dfe1fcff3
dishabl <dishabl96@gmail.com>
2024-01-12 00:28:12 +0300

    404 upd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Frame from "./Frame";
 import Register from "./Register";
 import Log from "./Log";
